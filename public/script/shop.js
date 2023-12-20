fca095f5913e78cf69dce732dcb04567753f13d9
rian911 <riansofc@gmail.com>
2023-12-20 18:39:03 +0700

    error

--- public/script/shop.js
+++ public/script/shop.js
@@ -15,7 +15,9 @@ export function updateDisplayedProduct() {
 
   filteredProducts.forEach((product) => {
     shopHTML += `
-    <div class="xl:w-60 bg-pm1 lg:w-2/5 p-4 m-1 rounded-xl w-full max-h-[345px] transform transition-all hover:-translate-y-2 duration-300 shadow-lg hover:shadow-2xl hover:shadow-sec1">
+    <div class="js-add-to-detail xl:w-60 bg-pm1 lg:w-2/5 p-4 m-1 rounded-xl w-full max-h-[345px] transform transition-all hover:-translate-y-2 duration-300 shadow-lg hover:shadow-2xl hover:shadow-sec1" data-product-id="${
+      product.id
+    }">
       <a class="block relative h-48 rounded overflow-hidden">
         <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="${
           product.image
@@ -48,6 +50,35 @@ export function updateDisplayedProduct() {
   });
 
   document.querySelector('.js-shop').innerHTML = shopHTML;
+}
+
+document.addEventListener('click', function (event) {
+  const addToDetailClass = 'js-add-to-detail';
+  if (event.target.classList.contains(addToDetailClass)) {
+    // Get the product data from the clicked element
+    const productId = event.target.getAttribute('data-product-id');
+    const selectedProduct = product.find((product) => product.id === productId);
+
+    // Save the selected product data to local storage
+    localStorage.setItem('selectedProduct', JSON.stringify(selectedProduct));
+
+    // Redirect to detail.html
+    window.location.href = 'detail.html';
+  }
+});
+
+// Function to retrieve and display the selected product on detail.html
+export function displaySelectedProduct() {
+  const selectedProduct = JSON.parse(localStorage.getItem('selectedProduct'));
+
+  // Check if a product is selected
+  if (selectedProduct) {
+    // Update the HTML content on detail.html with the selected product data
+    document.querySelector('.title-font').textContent = selectedProduct.name;
+    document.querySelector('.text-gray-500').textContent =
+      selectedProduct.brand;
+    // Add code to update other elements with the selected product data
+  }
 }
 
 updateDisplayedProduct();
